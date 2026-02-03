3b5ea8720cd37feca6b1026baea5b718fd83d1dc
Nolan Sherman <nolanrsherman@gmail.com>
2026-02-03 08:19:58 -0300

    stubbed out a main package

---
+++ cmd/gcpemulators/main.go
@@ -0,0 +1,8 @@
+// This package is the main entry point for the gcpemulators command.
+// It is used to start the emulators for the various GCP services.
+package main
+
+// TODO
+func main() {
+
+}
